b1cc859ce61479d92518f03bbe7cccdf4ca5d19c
Julian Nowaczek <jnowaczek@users.noreply.github.com>
2022-04-13 22:21:14 -0500

    Some junk IDK

---
+++ vivado/ip_repo/export/drivers/byte_count_v1_0/src/xbyte_count_hw.h
@@ -0,0 +1,27 @@
+// ==============================================================
+// Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2021.2 (64-bit)
+// Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+// ==============================================================
+// control
+// 0x00 : reserved
+// 0x04 : reserved
+// 0x08 : reserved
+// 0x0c : reserved
+// 0x10 : Data signal of input_r
+//        bit 31~0 - input_r[31:0] (Read/Write)
+// 0x14 : Data signal of input_r
+//        bit 31~0 - input_r[63:32] (Read/Write)
+// 0x18 : reserved
+// 0x1c : Data signal of out_r
+//        bit 31~0 - out_r[31:0] (Read)
+// 0x20 : Control signal of out_r
+//        bit 0  - out_r_ap_vld (Read/COR)
+//        others - reserved
+// (SC = Self Clear, COR = Clear on Read, TOW = Toggle on Write, COH = Clear on Handshake)
+
+#define XBYTE_COUNT_CONTROL_ADDR_INPUT_R_DATA 0x10
+#define XBYTE_COUNT_CONTROL_BITS_INPUT_R_DATA 64
+#define XBYTE_COUNT_CONTROL_ADDR_OUT_R_DATA   0x1c
+#define XBYTE_COUNT_CONTROL_BITS_OUT_R_DATA   32
+#define XBYTE_COUNT_CONTROL_ADDR_OUT_R_CTRL   0x20
+
